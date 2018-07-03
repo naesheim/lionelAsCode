abd44efd0f6bc981cc30b74874ef8046e0b264b0
naesheim <hg.nesheim@gmail.com>
2018-07-03 15:38:43 +0200

    chchchchanges

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,7 @@ provider "google" {
 resource "google_container_cluster" "primary" {
   name = "${var.cluster_name}"
   zone = "${var.zone}"
+  network = "default"
   remove_default_node_pool = true
   lifecycle {
     ignore_changes = ["node_pool"]
